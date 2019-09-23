a5e61d75d552ec809af6d35c72ca06d05dcbe3da
CD Cabrera <ccabrera@redhat.com>
2019-09-23 17:38:47 -0400

    DEV git diff to avoid unnecessary auto deployments

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -10,22 +10,34 @@
   GREEN="\e[32m"
   YELLOW="\e[33m"
   NOCOLOR="\e[39m"
+  DIFF_CHECK=$(git diff | grep '^+' | grep -v '+++' | grep -v 'Last updated' | wc -l)
 
+  # filter out pull requests
   if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
     echo -e "${YELLOW}Exiting early, pull request${NOCOLOR}"
     exit 0;
   fi
 
+  # filter out forked repositories
   if [[ $REPO != *"$TRAVIS_REPO_SLUG"* ]]; then
     echo -e "${YELLOW}Exiting early, not master repository${NOCOLOR}"
     exit 0;
   fi
 
+  # if auto deploy active filter out timestamp only updates
+  if [ "${AUTO_DEPLOY}" = "true" ] && [[ "${DIFF_CHECK}" = "0" ]]; then
+    echo -e "${YELLOW}$(git diff --compact-summary){$NOCOLOR}"
+    echo -e "${YELLOW}Exiting early, no significant changes${NOCOLOR}"
+    exit 0;
+  fi
+
+  # filter out deployment tokens when active
   if [ "${AUTO_DEPLOY}" != "true" ] && [[ ! "$TRAVIS_COMMIT_MESSAGE" =~ "[DEPLOY]" ]]; then
     echo -e "${YELLOW}Exiting early, not a deployment${NOCOLOR}"
     exit 0;
   fi
 
+  # filter process for specific branch and CI stage
   if [[ "${TRAVIS_BRANCH}" = "${BRANCH}" ]] && [[ $TRAVIS_BUILD_STAGE_NAME == *"Build"* ]]; then
     set +x
     openssl aes-256-cbc \
@@ -54,6 +66,7 @@
     exit 0;
   fi
 
+  # fallback messaging
   echo -e "${YELLOW}Exiting, not ${BRANCH} branch or build stage${NOCOLOR}"
   exit 0;
 }
